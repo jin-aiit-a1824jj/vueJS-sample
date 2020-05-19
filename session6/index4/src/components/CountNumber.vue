71530307bf275068b6933d45eba364d862fd796d
JIN <a1824jj@aiit.ac.jp>
2020-05-19 07:43:08 +0000

    ready to use mix

--- session6/index4/src/components/CountNumber.vue
+++ session6/index4/src/components/CountNumber.vue
@@ -5,17 +5,13 @@
     <p>{{ subtitle | lowerCase | upperCase }}</p>
     <p>{{number}}</p>
     <button @click="number++">+1</button>
-    <CountNumber></CountNumber>
   </div>
 </template>
 
 <script>
-import CountNumber from "./CountNumber.vue";
-
 export default {
   data(){
     return {
-      tmpData: "hello",
       title: "Welcome to Tokyo",
       subtitle: "Tokyo is a great city",
       number: 0
@@ -24,23 +20,6 @@ export default {
   computed: {
     upperCaseTitle() {
       return this.title.toUpperCase();
-    }
-  },
-  components:{
-    CountNumber
-  },
-  directives: {
-    border: function(el, binding){
-      //this.tmpData = "can not use";
-      el.style.borderWidth = binding.value.width;
-      el.style.borderColor = binding.value.color;
-      el.style.borderStyle = binding.arg;
-      if(binding.modifiers.round) {
-        el.style.borderRadius = "0.5rem";
-      }
-      if(binding.modifiers.shadow) {
-        el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.26)";
-      }
     }
   },
   filters: {
