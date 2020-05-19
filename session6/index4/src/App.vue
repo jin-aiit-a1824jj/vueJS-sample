0ec5102f3159e3c6b14731895df4a9dec5cb7770
JIN <a1824jj@aiit.ac.jp>
2020-05-19 03:01:33 +0000

    slot - check scope css

--- session6/index4/src/App.vue
+++ session6/index4/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <LikeHeader>
+    <LikeHeader headerText="Hello">
       <h1>トータルのいいね数</h1>
       <h2>{{ number }}</h2>
     </LikeHeader> 
@@ -33,5 +33,9 @@ export default {
 <style scoped>
 div {
   border: 1px solid red;
+}
+
+h1 {
+  color: green;
 }
 </style>
