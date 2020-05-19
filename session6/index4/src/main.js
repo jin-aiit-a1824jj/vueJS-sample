8b238bf02ee5c37ee872923438025b4838432cfb
JIN <a1824jj@aiit.ac.jp>
2020-05-19 07:03:56 +0000

    custom directive using local components

--- session6/index4/src/main.js
+++ session6/index4/src/main.js
@@ -6,17 +6,17 @@ Vue.config.productionTip = false
 Vue.component("LikeNumber", LikeNumber);
 Vue.directive("border", {
   bind(el, binding, vnode){
-    console.log("border-bind");
+    //console.log("border-bind");
     //el.style.border = "solid black 2px"
-    el.style.borderWidth = binding.value.width;
-    el.style.borderColor = binding.value.color;
-    el.style.borderStyle = binding.arg;
-    if(binding.modifiers.round) {
-      el.style.borderRadius = "0.5rem";
-    }
-    if(binding.modifiers.shadow) {
-      el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.26)";
-    }
+    // el.style.borderWidth = binding.value.width;
+    // el.style.borderColor = binding.value.color;
+    // el.style.borderStyle = binding.arg;
+    // if(binding.modifiers.round) {
+    //   el.style.borderRadius = "0.5rem";
+    // }
+    // if(binding.modifiers.shadow) {
+    //   el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.26)";
+    // }
   },
   inserted(el, binding, vnode){},
   update(el, binding, vnode, oldVnode){
