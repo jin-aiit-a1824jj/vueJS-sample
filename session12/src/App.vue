3a38692ad6bcc9524d09bb06df4441d67bec5b90
JIN <a1824jj@aiit.ac.jp>
2020-05-19 08:56:44 +0000

    css trasition and animation

--- session12/src/App.vue
+++ session12/src/App.vue
@@ -5,14 +5,8 @@
       <p v-if="show">hello</p>
     </transition>
     
-    <transition name="slide">
-      <div v-if="show">
-        <p>bye</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-      </div>
+    <transition name="slide" type="animation">
+        <p v-show="show">bye</p>
     </transition>
 
   </div>
@@ -53,13 +47,19 @@ export default {
   opacity: 0;
 }
 
+.slide-enter,
+.slide-leave-to{
+  opacity: 0;
+}
 
 .slide-enter-active{
   animation: slide-in 0.5s;
+  transition: opacity 0.5s;
 }
 
 .slide-leave-active{
   animation: slide-in 0.5s reverse;
+  transition: opacity 0.5s;
 }
 
 @keyframes slide-in {
