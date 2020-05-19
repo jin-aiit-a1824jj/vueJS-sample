56c15d3dda5220d5fbd13aa4c3547079fb01364d
JIN <a1824jj@aiit.ac.jp>
2020-05-19 07:01:49 +0000

    custom directive using binding pass to parameter &  modifiers

--- session6/index4/src/main.js
+++ session6/index4/src/main.js
@@ -11,6 +11,12 @@ Vue.directive("border", {
     el.style.borderWidth = binding.value.width;
     el.style.borderColor = binding.value.color;
     el.style.borderStyle = binding.arg;
+    if(binding.modifiers.round) {
+      el.style.borderRadius = "0.5rem";
+    }
+    if(binding.modifiers.shadow) {
+      el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.26)";
+    }
   },
   inserted(el, binding, vnode){},
   update(el, binding, vnode, oldVnode){
