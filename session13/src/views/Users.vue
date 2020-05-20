daed0e8a2e6e0223577ee9f26bf6a45f50367972
JIN <a1824jj@aiit.ac.jp>
2020-05-20 02:47:27 +0000

    using v-bind:to - create dynamic url

--- session13/src/views/Users.vue
+++ session13/src/views/Users.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <h3>Users</h3>
-    <router-link to=/users/1>ユーザー１</router-link>
-    <router-link to=/users/2>ユーザー２</router-link>
+    <router-link to="/users/1">ユーザー１</router-link>
+    <router-link to="/users/2">ユーザー２</router-link>
     <hr>
     <h1>User No. {{ id }} </h1>
+    <router-link :to=" '/users/' + (Number(id) + 1) + '/profile' ">次のユーザー</router-link>
     <router-view></router-view>
   </div>
 </template>
