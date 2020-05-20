a22e832aa4fd1b8975800de7d670c320eecf77ef
JIN <a1824jj@aiit.ac.jp>
2020-05-20 08:00:23 +0000

    file division - module sample

---
+++ session13/src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Vuex from "vuex";
+import count from "./modules/count.js";
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    message: ""
+  },
+  getters: {
+    message: state => state.message
+  },
+  mutations: {
+    updateMessage(state, newMessage){
+      state.message = newMessage
+    }
+  },
+  actions: {
+    updateMessage({commit}, newMessage){
+      commit('updateMessage', newMessage);
+    }
+  },
+  modules: {
+    count
+  }
+});
