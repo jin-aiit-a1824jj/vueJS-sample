e269568849c4a14cc08a64e2595812fc8ab78c0a
JIN <a1824jj@aiit.ac.jp>
2020-05-20 08:05:42 +0000

    file division - file function

--- session13/src/store/index.js
+++ session13/src/store/index.js
@@ -1,25 +1,20 @@
 import Vue from 'vue';
 import Vuex from "vuex";
 import count from "./modules/count.js";
+
+import getters from "./getters.js";
+import mutations from "./mutations.js";
+import actions from "./actions.js";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     message: ""
   },
-  getters: {
-    message: state => state.message
-  },
-  mutations: {
-    updateMessage(state, newMessage){
-      state.message = newMessage
-    }
-  },
-  actions: {
-    updateMessage({commit}, newMessage){
-      commit('updateMessage', newMessage);
-    }
-  },
+  getters: getters,
+  mutations,
+  actions,
   modules: {
     count
   }
