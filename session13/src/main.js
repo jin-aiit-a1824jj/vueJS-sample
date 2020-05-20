3808671d4c3c27f0fd9225c20182752c9dc54d5c
JIN <a1824jj@aiit.ac.jp>
2020-05-20 04:39:20 +0000

    using beforeEach / change componet before process

--- session13/src/main.js
+++ session13/src/main.js
@@ -4,6 +4,19 @@ import router from './router.js'
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next)=>{
+  console.log('global-beforeEach');
+  console.log(to);
+  console.log(from);
+
+  if(to.path === '/users/1'){
+    next("/");
+    //next(false);
+  }
+
+  next();
+});
+
 new Vue({
   router,
   render: h => h(App),
